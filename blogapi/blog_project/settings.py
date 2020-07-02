407d9a321b21e68b88d1821e0f61975607e4293d
Eric Omine <eric.omine@gmail.com>
2020-07-02 20:29:21 -0300

    gitignore fix

--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -44,20 +44,25 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000', # Default React port
+    'http://localhost:8000', # Default Django port
+)
 
 ROOT_URLCONF = 'blog_project.urls'
 
