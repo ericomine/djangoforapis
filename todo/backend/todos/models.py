0159790849051cbeacb87bdc8f0986cdee64896d
Eric Omine <eric.omine@gmail.com>
2020-07-02 16:33:35 -0300

    experimenting with m:n relationships in api

--- todo/backend/todos/models.py
+++ todo/backend/todos/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    user = models.ManyToManyField(User)
 
     def __str__(self):
         return self.title
